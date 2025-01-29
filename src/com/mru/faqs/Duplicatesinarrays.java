c025203946e4dae58a847168b4a0679b807237a2
amshumanth <amshumanthpenchala@gmail.com>
2025-01-29 19:26:47 +0530

    searching_techniques

---
+++ src/com/mru/faqs/Duplicatesinarrays.java
@@ -0,0 +1,23 @@
+package com.mru.faqs;
+
+import java.util.HashSet;
+
+public class Duplicatesinarrays {
+
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,1,2,3};
+		HashSet<Integer> set = new HashSet<>();
+		boolean flag = false;
+		for(int element:arr) {
+			if(set.contains(element)) {
+				flag = true;
+				break;
+			}else {
+				set.add(element);
+			}
+		}
+		System.out.println(flag ? "Duplicates are available" : "Duplicates are not found");
+
+	}
+
+}
